afe590608cc0cd567b1c0e1a55a66f561732f116
llama1412 <llamaking1337@gmail.com>
2019-07-13 20:07:23 +0100

    First working build

--- chat_server.py
+++ chat_server.py
@@ -1,14 +1,46 @@
 import socket
 import threading
+import time
 
-connected_ips = []
+connected_ips = {}
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server.bind(("0.0.0.0",9999))
 
+def clean_ips():
+	while True:
+		time.sleep(10)
+		global connected_ips
+		new_connected_ips = {}
+		print("[*] There are "+str(len(connected_ips))+" clients connected.")
+		for key in connected_ips:
+			last_time = connected_ips[key][0]
+			delta = time.time() - last_time
+			if delta > 10:
+				print("Removing "+str(key))
+			else:
+				new_connected_ips[key] = [last_time, connected_ips[key][0]]
+		connected_ips = new_connected_ips
+
+cleaner = threading.Thread(target=clean_ips)
+cleaner.daemon = True
+cleaner.start()
+
+def handle_message(message, sender):
+	for key in connected_ips:
+		if sender[0] != key:
+			server.sendto(message.encode(),(key, 9998))
+
+
 while True:
 	data, addr = server.recvfrom(4096)
-	if addr not in connected_ips:
-		connected_ips.append(addr)
-		print("[*] New connection from " + str(addr[0])+":"+str(addr[1]))
+	if addr[0] not in connected_ips.keys() and data.decode().startswith("I am "):
+		name = " ".join(data.decode().split(" ")[2:])
+		print("[*] New connection from " + str(addr[0])+":"+str(addr[1]) + " called "+name)
+		connected_ips[str(addr[0])] = [time.time(), name]
+	else:
+		connected_ips[str(addr[0])] = [time.time(), connected_ips[str(addr[0])][1]]
+		message = data.decode()
+		print(message)
+		threading.Thread(target=handle_message, args=(message,addr)).start()
--- udp_client.py
+++ udp_client.py
@@ -1,10 +1,31 @@
 import socket
+import threading
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 target_ip = "127.0.0.1"
 target_port = 9999
 
+receiver  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+receiver.bind(("0.0.0.0",9998))
 
-msg = "hello world"
+def handle_incoming(message):
+    print(message.decode())
+
+def await_messages():
+    while True:
+        message, addr = receiver.recvfrom(4096)
+        threading.Thread(target=handle_incoming, args=(message,)).start()
+
+# Sign into the server.
+name = input("What is your name?\t")
+msg = "I am " +str(name)
 client.sendto(msg.encode(),(target_ip,target_port))
+
+waiter = threading.Thread(target=await_messages)
+waiter.daemon = True
+waiter.start()
+while True:
+    msg = input()
+    formatted = name + ":\t" + msg
+    client.sendto(formatted.encode(),(target_ip,target_port))
